0c6b50f894b8d049c15fcdf1353cc7f018105134
Zach Kusz <zack.kusz@capitollien.com>
2024-04-07 17:00:12 -0500

    add form controls

--- src/views/ManageFilters.jsx
+++ src/views/ManageFilters.jsx
@@ -1,7 +1,79 @@
+import { For, Show, createSignal } from 'solid-js';
+import { createStore } from 'solid-js/store';
+
 export default function ManageFilters() {
+    const [filters, setFilters] = createStore([]);
+    const [newFilterName, setNewFilterName] = createSignal('');
+    const [newFilterType, setNewFilterType] = createSignal('keyword'); // keyword | username
+
+    // TODO handle empty name
+    const addFilter = (event) => {
+        event.preventDefault();
+
+        const newFilter = {
+            id: self.crypto.randomUUID(),
+            value: newFilterName(),
+            type: newFilterType(),
+        };
+        setFilters([...filters, newFilter]);
+        setNewFilterName('');
+    };
+
+    const deleteFilter = (id) => {
+        const nextFilters = filters.filter((filter) => {
+            return filter.id !== id;
+        });
+        setFilters(nextFilters);
+    };
+
     return (
         <>
             <h2>Manage Filters</h2>
+
+            <form onSubmit={addFilter}>
+                <label for="new-filter">Filter</label>
+                <input name="new-filter" autoComplete='off'
+                    value={newFilterName()} onInput={(e) => setNewFilterName(e.target.value)} />
+
+                <fieldset>
+                    <legend>Type</legend>
+                    <label for="keyword">
+                        Keyword
+                        <input type="radio" name="keyword" value="keyword" id="keyword"
+                            onInput={(e) => setNewFilterType(e.target.value)}
+                            checked={newFilterType() === 'keyword'}
+                        />
+                    </label>
+
+                    <label for="username">
+                        Username
+                        <input type="radio" name="username" value="username" id="username"
+                            onInput={(e) => setNewFilterType(e.target.value)}
+                            checked={newFilterType() === 'username'}
+                        />
+                    </label>
+                </fieldset>
+
+                <button type="submit">Submit</button>
+            </form>
+            <hr />
+
+            <h3>Filters:</h3>
+            <ul>
+                <For each={filters}>
+                    {(filter) => {
+                        return (
+                            <li>
+                                {filter.value} | {filter.type}
+                                <button type="button" onClick={[deleteFilter, filter.id]} class="filter-delete">Delete</button>
+                            </li>
+                        );
+                    }}
+                </For>
+            </ul>
+            <Show when={filters.length < 1}>
+                <p>No active filters</p>
+            </Show>
         </>
     );
 };
