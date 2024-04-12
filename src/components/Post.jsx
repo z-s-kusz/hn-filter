e4647a4240e47f2b5dcdd23aefb88d394f9a3071
Zach Kusz <z.s.kusz@gmail.com>
2024-04-12 15:02:51 -0500

    fix hn posts without links (jobs, ask etc;)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,25 +1,26 @@
 export default function Post(props) {
     const hnCommentUrl = 'https://news.ycombinator.com/item?id=' + props.post.id;
+    const link = props.post.url || hnCommentUrl;
 
     const getBaseUrl = (url) => {
         if (!url) return '';
 
-        const pathArray = url.split( '/' );
+        const pathArray = url.split('/');
         const protocol = pathArray[0];
         const host = pathArray[2];
         const baseUrl = protocol + '//' + host;
         return baseUrl;
     };
 
-    const baseUrl = getBaseUrl(props.post.url);
+    const baseUrl = getBaseUrl(link);
 
     return (
         <div class="post">
-            <a href={props.post.url}>
+            <a href={link}>
                 {props.post.title}
             </a>
             <div class="post-line-2">
-                <span class="baseurl">{baseUrl} </span>
+                <span class="baseurl">{baseUrl}</span>
                 <a class="comments" href={hnCommentUrl}>View Comments</a>
             </div>
         </div>
