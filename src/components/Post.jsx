29281d034c79d10de2c1847280566e3b6447915b
Zach Kusz <z.s.kusz@gmail.com>
2024-07-16 21:38:51 -0500

    show pdf in title if author didn't add it

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,6 +2,17 @@ export default function Post(props) {
     const hnCommentUrl = 'https://news.ycombinator.com/item?id=' + props.post.id;
     const link = props.post.url || hnCommentUrl;
 
+    // affix file type if it is not declared in title
+    // TODO handle others? I'm not sure HN allows other download types. Only seen pdf.
+    const downloadType = (url, title) => {
+        if (!url) return '';
+
+        if (url.includes('.pdf') && !title.toLowerCase().includes('[pdf]')) {
+            return ' [pdf]';
+        } 
+        return '';
+    }
+
     const getBaseUrl = (url) => {
         if (!url) return '';
 
@@ -12,15 +23,13 @@ export default function Post(props) {
         return baseUrl;
     };
 
-    const baseUrl = getBaseUrl(link);
-
     return (
         <div class="post">
             <a href={link}>
-                {props.post.title}
+                {props.post.title}{downloadType(link, props.post.title)}
             </a>
             <div class="post-line-2">
-                <span class="baseurl">{baseUrl}</span>
+                <span class="baseurl">{getBaseUrl(link)}</span>
                 <a class="comments" href={hnCommentUrl}>View Comments</a>
             </div>
         </div>
