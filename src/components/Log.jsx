efc66043315d5d996171984149b8c0628858da81
z-s-kusz <z.s.kusz@gmail.com>
2024-10-14 19:12:26 -0500

    make results list more concise and compact

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,16 +1,22 @@
-import { For } from 'solid-js';
-
 export default function Log(props) {
+    let positiveCount = () => getCountSummary(props.log, 'positive');
+    let negativeCount = () => getCountSummary(props.log, 'negative');
+    let neutralCount  = () => getCountSummary(props.log, 'neutral');
+
+    const getCountSummary = (logObject, category) => {
+        let count = 0;
+        logObject.analysis.forEach((analysis) => {
+            if (analysis === category) count++;
+        });
+        return count;
+    };
+
     return (
-        <details>
-            <summary>{props.log.text}</summary>
-            <ol>
-                <For each={props.log.analysis}>
-                    {(analysis) => {
-                        return <li>{analysis}</li>
-                    }}
-                </For>
-            </ol>
+        <details class="log-details">
+            <summary>{props.log.text} ({props.log.postId})</summary>
+            <p>Positive: {positiveCount()}</p>
+            <p>Negative: {negativeCount()}</p>
+            <p>Neutral: {neutralCount()}</p>
         </details>
     );
 }
