6ddd6f71678a0f0b84b6597187ce515d91cde4f1
z-s-kusz <z.s.kusz@gmail.com>
2025-10-01 14:39:38 -0500

    add padding in logs and links headers

--- src/components/ExtraLinks.jsx
+++ src/components/ExtraLinks.jsx
@@ -14,11 +14,11 @@ export default function ExtraLinks() {
             <Transition name="drawer">
                 <Show when={open()}>
                     <div class="drawer" onClick={toggleOpen}>
-                        <p class="sub-header">Newsletters</p>
+                        <h5 class="sub-header">Newsletters</h5>
                         <a href="/frontend-focus">Frontend Focus</a>
                         <a href="/js-weekly">JS Weekly</a>
 
-                        <p class="sub-header">Other Links</p>
+                        <h5 class="sub-header">Other Links</h5>
                         <a href="/logs">Logs</a>
                         <Show when={import.meta.env.DEV}>
                             <a href="/llm-test">LLM Test</a>
